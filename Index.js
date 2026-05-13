d9a2308e81caaa230649c68a5eb3dd4ff64bba65
Mylaka Stokes <mylakasttt@gmail.com>
2026-05-12 19:13:29 -0500

    i fucked up a lot of shit

--- Index.js
+++ Index.js
@@ -4,23 +4,25 @@
 const nudgeButton = document.getElementById("Nudge");
 const notif = document.getElementById("notif");
 const nudgeSent = document.getElementById("nudgeSent");
-
 nudgeButton.addEventListener("click", async function() {
     nudgeButton.innerHTML = "Nudge Sent!"
     function nudgeTimer(){
-        nudgeButton.innerHTML = "Send Nudge"
-    }
+        nudgeButton.innerHTML = "Nudge A Friend!"
+        audio.play();
     setTimeout(nudgeTimer, 3000);
+    }
 
     // Notification code
     const reg = await navigator.serviceWorker.register('./serviceWorker.js');
     await Notification.requestPermission();
-    reg.showNotification('GO! Its Your Turn!', { body: 'Goooooooo' });
-});
+    reg.showNotification('GO! Its Your Turn!', { 
+        body: "Goooooooo", 
+        icon: "goapppic.png"
+    });
 
 
 
 
 
 
-
+    });
